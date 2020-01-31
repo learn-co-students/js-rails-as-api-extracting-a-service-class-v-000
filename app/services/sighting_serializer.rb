9c42bf80dbae3c786a9f07bd84e8457eedf2cadf
scottms724 <scottms724@gmail.com>
2020-01-31 13:11:15 -0500

    Done.

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -4,4 +4,19 @@ class SightingSerializer
         @sighting = sighting_object
     end 
     
+    def to_serialized_json
+        options = {
+          include: {
+            bird: {
+              only: [:name, :species]
+            },
+            location: {
+              only: [:latitude, :longitude]
+            }
+          },
+          except: [:updated_at],
+        }
+        @sighting.to_json(options)
+      end
+
 end 
