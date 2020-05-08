d6e30a630c2eb972d0d8a86921736dd0294f154c
iamtamtamiam <vutam_a@hotmail.com>
2020-05-08 13:29:17 -0500

    Done.

---
+++ app/services/sighting_serializer.rb
@@ -0,0 +1,20 @@
+class SightingSerializer
+
+    def index
+        sightings = Sighting.all
+        render json: sightings.to_json(:include => {
+          :bird => {:only => [:name, :species]},
+          :location => {:only => [:latitude, :longitude]}
+        }, :except => [:updated_at])
+      end
+     
+      def show
+        sighting = Sighting.find_by(id: params[:id])
+        render json: sighting.to_json(:include => {
+          :bird => {:only => [:name, :species]},
+          :location => {:only => [:latitude, :longitude]}
+        }, :except => [:updated_at])
+      end
+    end
+
+end
